ddbe7a9d035383e2d9ca4e1cd0a507ba8c8f037f
fr0xy <rchoffar@student.42.fr>
2020-04-30 13:58:57 +0200

    rm D01

--- D01/Ex03/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "ZombieHorde.hpp"
-#include "Zombie.hpp"
-
-int main() {
-    ZombieHorde Horde = ZombieHorde(10);
-    Horde.advertAll();
-    return (0);
-}
--- D01/Ex05/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Human.hpp"
-
-int main()
-{
-    Human bob;
-    std::cout << bob.identify() << std::endl;
-    std::cout << bob.getBrain().identify() << std::endl;
-}
